957ef3c76ed474e9146613d5229ea26faac60938
JangHyunsoo <hunsoo911@gmail.com>
2022-11-16 15:20:29 +0900

    add PlayerMovement

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private NavMeshAgent navmesh_agent_;
+
+    private Vector3 target_pos_;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        navmesh_agent_ = GetComponent<NavMeshAgent>();
+        navmesh_agent_.acceleration = 1000f;
+        navmesh_agent_.speed = 4;
+        target_pos_ = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            var mouse_pos = Input.mousePosition;
+            Ray ray = Camera.main.ScreenPointToRay(mouse_pos);
+            if(Physics.Raycast(ray, out RaycastHit raycastHit, 100f))
+            {
+                target_pos_ = raycastHit.point;
+            }
+        }
+
+        navmesh_agent_.SetDestination(target_pos_);
+    }
+}
